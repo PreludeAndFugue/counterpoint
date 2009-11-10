799f0cd19fa80a1359d1d4caa648591137b0ce26
Gary Kerr <gdrummondk@gmail.com>
2009-11-10 19:35:24 +0000

    Test assertions each have their own method.

--- test/note_test.py
+++ test/note_test.py
@@ -7,22 +7,34 @@ class TestNoteCreation(unittest.TestCase):
         self.dsharp = Note('D', 'sharp', 5, 2)
         self.eflat = Note('E', 'flat', 3, 8)
         
-    def test_str(self):
+    def test_str_c(self):
         self.assertEqual(str(self.c), 'C')
+        
+    def test_str_d(self):
         self.assertEqual(str(self.dsharp), 'D#')
+        
+    def test_str_e(self):
         self.assertEqual(str(self.eflat), 'Eb')
         
     def test_repr(self):
         pass
         
-    def test_octave(self):
+    def test_octave_c(self):
         self.assertEqual(self.c.octave, 4)
+    
+    def test_octave_d(self):
         self.assertEqual(self.dsharp.octave, 5)
+        
+    def test_octave_e(self):
         self.assertEqual(self.eflat.octave, 3)
         
-    def test_duration(self):
+    def test_duration_c(self):
         self.assertEqual(self.c.duration, 16)
+        
+    def test_duration_d(self):
         self.assertEqual(self.dsharp.duration, 2)
+        
+    def test_duration_e(self):
         self.assertEqual(self.eflat.duration, 8)
         
 if __name__ == '__main__':
--- test/rest_test.py
+++ test/rest_test.py
@@ -7,19 +7,31 @@ class TestRestCreation(unittest.TestCase):
         self.r32 = Rest(32)
         self.r1 = Rest(1)
         
-    def test_repr(self):
+    def test_repr_16(self):
         self.assertEqual(repr(self.r16), 'Rest(16)')
+        
+    def test_repr_32(self):
         self.assertEqual(repr(self.r32), 'Rest(32)')
+        
+    def test_repr_1(self):
         self.assertEqual(repr(self.r1), 'Rest(1)')
         
-    def test_str(self):
+    def test_str_16(self):
         self.assertEqual(str(self.r16), 'crotchet rest')
+    
+    def test_str_32(self):
         self.assertEqual(str(self.r32), 'minim rest')
+        
+    def test_str_11(self):
         self.assertEqual(str(self.r1), 'hemi-demi-semiquaver rest')
         
-    def test_duration(self):
+    def test_duration_16(self):
         self.assertEqual(self.r16.duration, 16)
+        
+    def test_duration_32(self):
         self.assertEqual(self.r32.duration, 32)
+        
+    def test_duration_11(self):
         self.assertEqual(self.r1.duration, 1)
         
 if __name__ == '__main__':
